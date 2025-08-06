6328f41324028400f403e65629e595f4023c8cc0
Sirojiddin7_7_7 <bahtiyorovsirojiddin333@gmail.com>
2025-08-06 16:57:38 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+      
+   <h2> Welcome to React JS</h2>
+  <h3>This is my first React app </h3>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>Javascript</li>
+      <li> React</li>
+    </ul>
+  
+  <h2>What is React</h2>
+  React nima? React — bu Facebook tomonidan yaratilgan ochiq kodli JavaScript kutubxonasi
+  bo'lib,<br /> asosan foydalanuvchi interfeyslarini (UI) yaratish uchun mo'ljallangan. U yagona sahifali
+  ilovalar (single-page applications)<br /> va dinamik, sezgir veb-ilovalarni ishlab chiqishda keng
+  qo llaniladi.
+  <h2>What is Single-page application(SPA)</h2>
+  SPA (Single Page Application) — bu bitta sahifali veb-ilova bo lib, unda sahifa kontenti dinamik
+  ravishda yangilanadi <br /> va foydalanuvchining har bir amalida butun sahifani qaytadan yuklash
+  talab qilinmaydi. JavaScript yordamida faqat<br /> kerakli kontent o' zgartiriladi, bu esa ilovani tezkor
+  va foydalanuvchi uchun qulay qiladi.
+    </>
+  )
+}
+
+export default App;
